5879451261c9361cffb94588c5804aa8cf5e058c
Alex Cota <cota.alexandra14@gmail.com>
2017-09-18 16:58:40 -0700

    more changes to client

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -26,3 +26,51 @@ class Articles extends Component {
   	)
   	.catch(err => console.log(err));
   };
+
+  deleteArticle = id => {
+  	API.deleteArticle(id)
+  	.then(res => this.loadArticles())
+  	.catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+  	const { name, value } = event.target;
+  	this.setState({
+  		[name]: value
+  	});
+  };
+	 render() {
+	 	return (
+	 		<Container fluid>
+				<Row>
+					<Col size = 'md-12'>
+						<Jumbotron>
+							<h1>NYT React</h1>
+						</Jumbotron>
+						{this.state.articles.length ? (
+							<List>
+								{this.state.articles.map(article => {
+									return (
+										<ListItem key={article._id}>
+											<a href={'/articles/' + article._id}>
+												<strong>
+													{article.title} at (article.url)
+												</strong>
+											</a>
+											<DeleteBtn onClick={() => this.deleteArticle(article._id)} />
+										</ListItem>
+									);
+								})}
+							</List>
+							) : (
+								<h3>No Results to Display</h3>
+							)}
+					</Col>
+				</Row>
+			</Container> 		
+	 	);
+	 }
+}
+
+export default Articles;
+
