3ddecf865c105f907630d1fcb80e2423ad08c5d8
Alex Cota <cota.alexandra14@gmail.com>
2017-09-19 21:35:15 -0700

    add component files

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
-app.use(express.static("./client/build"));
+app.use(express.static("./client/src/index.js"));
 // Add routes, both API and view
 app.use(routes);
 
