a9af7c29a64c42dd2dfe96e9a5272a66af7b5a2a
Alex Cota <cota.alexandra14@gmail.com>
2017-11-06 10:55:51 -0800

    saved.js

---
+++ client/src/components/Saved.js
@@ -0,0 +1,104 @@
+import React from "react";
+
+var helpers=require("../utils/helpers");
+
+var Main = React.createClass({
+	getInitialState: function() {
+		return { savedArticles: "" };
+	},
+
+	componentDidMount: function() {
+		helpers.getSaved().then(function(articleData) {
+			this.setState({ savedArticles: articleData.data });
+			console.log("saved results", articleData.data);
+		}.bind(this));	
+	},
+
+	handleClick: function(item) {
+		console.log("Clicked");
+		console.log(item);
+
+		//Delete the list
+		helpers.deleteSaved(item.title, item.date, item.url).then(function() {
+			
+			//Get revised list
+			helpers.getSaved().then(function(articleData) {
+				this.setState({ savedArticles: articleData.data });
+				console.log("saved results", acticleData.data);
+			}.bind(this));
+		}.bind(this));
+	},
+
+	//This is a helper method for rendering the HTML when we have no saved articles
+	renderEmpty: function() {
+		return (
+			<li className="list-group-item">
+				<h3>
+					<span>
+						<em>Save your first article</em>
+					</span>
+				</h3>
+			</li>
+		);
+	},
+
+//This is a helper method for mapping through our articles and outputting HTML
+renderArticles: function() {
+	return this.state.savedArticles.map(function(article, index) {
+
+		return (
+			<div key={index}>
+				<li className="list-group-item">
+					<h3>
+						<span>
+							<em>{article.title}</em>
+						</span>
+						<span className="btn-group pull-right">
+							<a href={article.url} rel="noopener noreferrer" target="blank">
+								<button className="btn btn-default">View Article</button>
+							</a>
+							<button className="btn btn-primary" onClick={() => this.handleClick(article)}>Delete</button>
+						</span>
+					</h3>
+					<p>Date published: {article.date}</p>
+				</li>
+			</div>
+			);
+	}.bind(this));
+},
+
+//This is a helper method for rendering a container and all of our articles inside
+renderContainer: function() {
+	return (
+		<div className="main-container">
+			<div className="row">
+				<div className="col-md-12">
+					<div className="panel panel-primary">
+						<div className="panel-heading">
+							<h1 className="panel-title">
+								Saved Articles
+							</h1>
+						</div>
+						<div className="panel-body">
+							<ul className="list-group">
+								{this.renderArticles()}
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		);
+	},
+
+	render: function() {
+		//if there are no articles, return this.renderEmpty()
+		if (!this.state.savedArticles) {
+			return this.renderEmpty();
+		}
+		return this.renderContainer();
+	}
+
+});
+
+module.exports = Main;
